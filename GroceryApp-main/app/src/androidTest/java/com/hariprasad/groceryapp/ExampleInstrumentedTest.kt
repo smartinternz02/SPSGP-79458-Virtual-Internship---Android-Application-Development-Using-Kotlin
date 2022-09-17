981b26fe7613dadd6a94aaf021d53dd605f21e10
hariprasad2651 <107412204+hariprasad2651@users.noreply.github.com>
2022-09-17 21:26:40 +0530

    Update ExampleInstrumentedTest.kt

--- GroceryApp-main/app/src/androidTest/java/com/hariprasad/groceryapp/ExampleInstrumentedTest.kt
+++ GroceryApp-main/app/src/androidTest/java/com/hariprasad/groceryapp/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.divyanshu.groceryapp
+package com.hariprasad.groceryapp
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
