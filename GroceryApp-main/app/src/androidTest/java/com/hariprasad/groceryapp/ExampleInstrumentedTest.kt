a15f1bb909aebbc9fe9c9fa603c5574b087eca42
hariprasad2651 <107412204+hariprasad2651@users.noreply.github.com>
2022-09-17 21:40:47 +0530

    Update ExampleInstrumentedTest.kt

--- GroceryApp-main/app/src/androidTest/java/com/hariprasad/groceryapp/ExampleInstrumentedTest.kt
+++ GroceryApp-main/app/src/androidTest/java/com/hariprasad/groceryapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.divyanshu.groceryapp", appContext.packageName)
+        assertEquals("com.hariprasad.groceryapp", appContext.packageName)
     }
 }
